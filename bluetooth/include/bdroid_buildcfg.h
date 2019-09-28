31c88495628bafaea5915fd4b638dd3c9f9a7eac
Pranav Vashi <neobuddy89@gmail.com>
2019-09-28 23:17:44 +0530

    cheeseburger: Clean up bdroid header
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- bluetooth/include/bdroid_buildcfg.h
+++ bluetooth/include/bdroid_buildcfg.h
@@ -23,12 +23,10 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME "OnePlus 5"
-#define BLUETOOTH_QTI_SW TRUE
-// Disables read remote device feature
+#define BTM_DEF_LOCAL_NAME "OnePlus 5T"
+
+#define BLE_VND_INCLUDED   TRUE
 #define MAX_ACL_CONNECTIONS   16
 #define MAX_L2CAP_CHANNELS    16
-#define BLE_VND_INCLUDED   TRUE
-// skips conn update at conn completion
-#define BT_CLEAN_TURN_ON_DISABLED 1
+
 #endif
